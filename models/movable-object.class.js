15d6466bd74e296977f34da8659a3ca5358b1aed
lucas <lucashamacher01@gmail.com>
2025-04-25 11:05:45 +0200

    add collision tracker

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -27,7 +27,20 @@ class MovableObject {
             img.src = path;
             this.imageCache[path] = img;
         });
+    }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
     }
 
     moveRight() {
@@ -66,13 +79,11 @@ class MovableObject {
         this.speedY = 30;
     }
 
-    // setAnimationTime(newTime) {
-    //     if (newTime) {
-    //         this.animationTime = newTime;
-    //         console.log(this.animationTime)
-    //     } else {
-    //         this.animationTime = 100;
-    //     }
-    // }
+    isColliding(mo){
+        return this.x + this.width > mo.x &&
+        this.y + this.height > mo.y &&
+        this.x < mo.x &&
+        this.y < mo.y + mo.height;
+    }
 
 }
--- models/world.class.js
+++ models/world.class.js
@@ -12,6 +12,7 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
+        this.checkCollisions();
     }
 
 
@@ -36,25 +37,46 @@ class World {
 
     addObjectsToMap(objects) {
         objects.forEach(o => {
-            this.addToMap(o)
+            this.addToMap(o);
         })
     }
 
     addToMap(mo) {
         if(mo.otherDirection){
-            this.ctx.save();
-            this.ctx.translate(mo.width, 0);
-            this.ctx.scale(-1, 1);
-            mo.x = mo.x * -1;
+            this.flipImage(mo)
         }
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
+        mo.draw(this.ctx)
+        mo.drawFrame(this.ctx)
+
         if(mo.otherDirection){
-            mo.x = mo.x * -1;
-            this.ctx.restore();
+            this.flipImageBack(mo)
         }
     }
 
     setWorld() {
         this.character.world = this;
+    }
+
+    flipImage(mo) {
+        this.ctx.save();
+        this.ctx.translate(mo.width, 0);
+        this.ctx.scale(-1, 1);
+        mo.x = mo.x * -1;
+    }
+
+    flipImageBack(mo) {
+        mo.x = mo.x * -1;
+        this.ctx.restore();
+    }
+
+    checkCollisions() {
+        setInterval(() => {
+            this.level.enemies.forEach((enemy) => {
+                if (this.character.isColliding(enemy)){
+                    console.log('collision with enemy', enemy)
+                }
+            });
+        }, 200)
+
     }
 }
